a3f5e8244af5df9e208b766e9477fcfb6f5cba2b
uakhponomarev <vic6090156@gmail.com>
2019-09-15 17:36:47 +0300

    +slick js , tour-menu js

--- dist/js/index.js
+++ dist/js/index.js
@@ -93,14 +93,44 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$('.header-bottom-wrapper .container').slick({
+// <<<<<<<<<<<<<< SETS after load the document
+var $tourLink = $('.tours-menu-link').eq(0);
+$(document).ready(function () {
+  // set active state to header-bottom slide dots
+  $('.slick-dots button').eq(0).addClass('active'); // set active state to tour-menu link
+
+  $tourLink.addClass('picked');
+}); // <<<<<<<<<<<<<<< HEADER_BOTTOM SLICK SLIDE >>>>>>>>>>>>>>>
+
+var $slickDotsContainer = $('.slick-dots');
+var $slickDotsButton = $('.slick-dots button'); // const $slickButton = $('.slick-dots button');
+
+var $containerSliders = $('.slider-holder'); // acitvate slick slide
+
+$containerSliders.slick({
   dots: true,
   arrows: false,
   infinite: true,
   mobileFirst: true,
   speed: 300
+}); // delete content of slick-slides li
+
+$('.slick-dots button').text(''); // add event to slick dots
+
+$('.slick-dots').click(function (e) {
+  if ($(e.target).prop("tagName") === 'BUTTON') {
+    $('.slick-dots button').removeClass('active');
+    $(e.target).addClass('active');
+  }
+}); // <<<<<<<<<<<<<<< TOUR_MENU  >>>>>>>>>>>>>>>
+// add event to tour-menu links
+
+$('.tours-menu').click(function (e) {
+  if ($(e.target).prop("tagName") === 'A') {
+    $('.tours-menu a').removeClass('picked');
+    $(e.target).addClass('picked');
+  }
 });
-$('.slick-dots li button').text('');
 
 /***/ })
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,46 @@
-$('.header-bottom-wrapper .container').slick({
-  dots: true,
-  arrows:false,
-  infinite: true,
-  mobileFirst:true,
-  speed: 300, 
+// <<<<<<<<<<<<<< SETS after load the document
+
+const $tourLink = $('.tours-menu-link').eq(0);
+$(document).ready(() => {
+  // set active state to header-bottom slide dots
+  $('.slick-dots button').eq(0).addClass('active');
+  // set active state to tour-menu link
+  $tourLink.addClass('picked');
 });
 
-$('.slick-dots li button').text('');
+// <<<<<<<<<<<<<<< HEADER_BOTTOM SLICK SLIDE >>>>>>>>>>>>>>>
+
+const $slickDotsContainer = $('.slick-dots');
+const $slickDotsButton = $('.slick-dots button');
+// const $slickButton = $('.slick-dots button');
+const $containerSliders = $('.slider-holder');
+
+// acitvate slick slide
+$containerSliders.slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  mobileFirst: true,
+  speed: 300,
+});
+
+// delete content of slick-slides li
+$('.slick-dots button').text('');
+
+// add event to slick dots
+$('.slick-dots').click((e) => {
+  if ($(e.target).prop("tagName") === 'BUTTON') {
+    $('.slick-dots button').removeClass('active');
+    $(e.target).addClass('active');
+  }
+});
+
+// <<<<<<<<<<<<<<< TOUR_MENU  >>>>>>>>>>>>>>>
+
+// add event to tour-menu links
+$('.tours-menu').click((e) => {
+  if ($(e.target).prop("tagName") === 'A') {
+    $('.tours-menu a').removeClass('picked');
+    $(e.target).addClass('picked');
+  }
+});
